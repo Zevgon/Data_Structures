ba26c2c762acdea54d1a0b9d6439d4245a5e8a20
Yale Work <yalework@Yale-Works-MacBook-Pro.local>
2016-11-21 21:02:43 -0800

    Add sort and delete methods

--- heap.py
+++ heap.py
@@ -10,43 +10,20 @@ class Heap:
 		children = [(idx + 1) * 2 - 1, (idx + 1) * 2]
 		return children
 
-	def heapify_store(self):
-		new_store = list(self.store)
-		self.store = []
-		for item in new_store:
-			self.insert(item)
+	def delete(self, idx):
+		if idx < 0 or idx >= len(self.store):
+			raise Exception('Cannot delete item: index out of range')
+		last = len(self.store) - 1
+		self.swap(idx, last)
+		self.pop()
+		self.heapify_up(idx)
+		self.heapify_down(idx)
 
-	def parent_idx(self, idx):
-		if idx % 2 == 0:
-			return idx / 2 - 1
-		else:
-			return idx / 2
+	def pop(self):
+		self.store.pop()
 
-	def insert(self, *items):
-		for item in items:
-			self.store.append(item)
-			self.heapify_up()
-
-	def heapify_up(self):
-		idx = len(self.store) - 1
-		parent_idx = self.parent_idx(idx)
-		while self.should_swap(parent_idx, idx):
-			self.swap(parent_idx, idx)
-			idx = parent_idx
-			parent_idx = self.parent_idx(idx)
-
-	def should_swap(self, parent_idx, idx):
-		if idx >= len(self.store) or parent_idx < 0:
-			return False
-		parent = self.store[parent_idx]
-		child = self.store[idx]
-		return not self.comparator(self.prc(parent), self.prc(child))
-
-	def swap(self, parent_idx, idx):
-		self.store[parent_idx], self.store[idx] = self.store[idx], self.store[parent_idx]
-
-	def show(self):
-		print self.store
+	def empty(self):
+		return not self.store
 
 	def extract(self, num = 1):
 		extracted_vals = []
@@ -63,14 +40,6 @@ class Heap:
 		else:
 			return extracted_vals
 
-	def heapify_down(self):
-		idx = 0
-		swap_with = self.get_child_swap(idx)
-		while swap_with:
-			self.swap(idx, swap_with)
-			idx = swap_with
-			swap_with = self.get_child_swap(idx)
-
 	def get_child_swap(self, idx):
 		block = lambda child_idx: self.should_swap(idx, child_idx)
 		options = filter(block, self.child_indices(idx))
@@ -86,11 +55,66 @@ class Heap:
 			else:
 				return options[1]
 
-	def empty(self):
-		return not self.store
+	def heapify_down(self, idx = 0):
+		swap_with = self.get_child_swap(idx)
+		while swap_with:
+			self.swap(idx, swap_with)
+			idx = swap_with
+			swap_with = self.get_child_swap(idx)
 
-# # h = Heap([8, 2, 4, 6, 7, 1, 2, 3], lambda x, y: x < y)
-# h = Heap([], lambda x, y: x > y)
-# h.insert(2, 5, 1, 6, 1)
-# print h.extract()
+	def heapify_store(self):
+		new_store = list(self.store)
+		self.store = []
+		for item in new_store:
+			self.insert(item)
+
+	def heapify_up(self, idx = None):
+		if not idx:
+			idx = len(self.store) - 1
+		parent_idx = self.parent_idx(idx)
+		while self.should_swap(parent_idx, idx):
+			self.swap(parent_idx, idx)
+			idx = parent_idx
+			parent_idx = self.parent_idx(idx)
+
+	def insert(self, *items):
+		for item in items:
+			self.store.append(item)
+			self.heapify_up()
+
+	ins = insert
+
+	def parent_idx(self, idx):
+		if idx % 2 == 0:
+			return idx / 2 - 1
+		else:
+			return idx / 2
+
+	def should_swap(self, parent_idx, idx):
+		if idx >= len(self.store) or parent_idx < 0:
+			return False
+		parent = self.store[parent_idx]
+		child = self.store[idx]
+		return not self.comparator(self.prc(parent), self.prc(child))
+
+	def show(self):
+		print self.store
+
+	def sort(self):
+		old_store = list(self.store)
+		result = []
+		while not self.empty():
+			result.append(self.extract())
+		self.store = old_store
+		return result
+
+	def swap(self, parent_idx, idx):
+		self.store[parent_idx], self.store[idx] = self.store[idx], self.store[parent_idx]
+
+
+# h = Heap([8, 2, 4, 6, 7, 1, 2, 3], lambda x, y: x < y)
+# h = Heap([], lambda x, y: x < y)
+# h.ins(2, 5, 1, 6, 1, 7, 3, 2, 4, 81, 72)
+# h.delete(6)
+# print h.sort()
 # h.show()
